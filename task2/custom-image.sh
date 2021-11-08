95ce73b1296e5bde1a1c9595d99410d7db296331
vinaymsv3 <92072514+vinaymsv3@users.noreply.github.com>
2021-11-08 14:52:44 +0530

    Update custom-image.sh

--- task2/custom-image.sh
+++ task2/custom-image.sh
@@ -1,4 +1,4 @@
 docker image build -t mywebserver .
-docker tag mywebserver gcr.io/amplified-asset-328305/my-web-server
-docker push gcr.io/amplified-asset-328305/my-web-server
-docker run -dp 8081:80 gcr.io/amplified-asset-328305/my-web-server
+docker tag mywebserver srebootcamp.azurecr.io/my-web-server
+docker push srebootcamp.azurecr.io/my-web-server
+docker run -dp 8081:80 srebootcamp.azurecr.io/my-web-server
